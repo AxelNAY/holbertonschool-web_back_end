746f25ec51a75de7f54a2b7ad723ed1a7c52beb1
AxelNAY <axelnay@sfr.fr>
2024-07-26 10:02:29 +0200

    Configuration npm/npx

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,20 +2,20 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-const userPromise = signUpUser(firstName, lastName);
-const photoPromise = uploadPhoto(fileName);
-const results = await Promise.allSettled([userPromise, photoPromise]);
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+  const results = await Promise.allSettled([userPromise, photoPromise]);
 
-return results.map((result) => {
-if (result.status === 'fulfilled') {
-return {
-status: 'fulfilled',
-value: result.value,
-};
-}
-return {
-status: 'rejected',
-value: result.reason,
-};
-});
+  return results.map((result) => {
+    if (result.status === 'fulfilled') {
+      return {
+        status: 'fulfilled',
+        value: result.value,
+      };
+    }
+    return {
+      status: 'rejected',
+      value: result.reason,
+    };
+  });
 }
