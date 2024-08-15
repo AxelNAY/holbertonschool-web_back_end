0919dec2f01b7fe10928a34715f6af2076e6faa1
AxelNAY <axelnay@sfr.fr>
2024-08-15 23:44:01 +0200

    Check

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -3,7 +3,8 @@
 (max_delay, with a default value of 10) named wait_random that waits for a
 random delay between 0 and max_delay (included and float value) seconds
 and eventually returns it.'''
-import asyncio, random
+import asyncio
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -2,7 +2,8 @@
 '''Create a measure_time function with integers n and max_delay as arguments
 that measures the total execution time for wait_n(n, max_delay),
 and returns total_time / n. Your function should return a float.'''
-import asyncio, time
+import asyncio
+import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
