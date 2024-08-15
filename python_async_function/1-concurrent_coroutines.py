806b76703b2bc16e4ee416759bde18dd19ae888f
AxelNAY <axelnay@sfr.fr>
2024-08-15 23:57:24 +0200

    Check

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,7 +10,9 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-
+    '''Write an async routine called wait_n that takes in 2 int arguments
+    (in this order): n and max_delay. You will spawn wait_random n times
+    with the specified max_delay.'''
     delay = [wait_random(max_delay) for i in range(0, n)]
     delay_list = await asyncio.gather(*delay)
     async_list = []
--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -10,7 +10,8 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-
+    '''Write an asynchronous coroutine that take the code from wait_n
+    and alter it into a new function task_wait_n.'''
     delay = [task_wait_random(max_delay) for i in range(0, n)]
     delay_list = await asyncio.gather(*delay)
     async_list = []
